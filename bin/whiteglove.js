5d5c7dbfbbef7a54685b32c20da0e9576a0b2d64
Ian Christian Myers <ian@iancmyers.com>
2016-09-21 14:30:59 -0700

    Fix issue with importing execute function

--- bin/whiteglove.js
+++ bin/whiteglove.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /* eslint import/no-unresolved:0 no-mixed-operators:0 */
-const execute = require('../lib/cli');
+const execute = require('../lib/cli').default;
 
 execute(process.argv, (run) => {
   if (!run || run && run.reportedTests().length > 0) {
